435f6ccb9d62d30a29874e849fdafde98d7e3bdb
anhphha <74095848+anhphha@users.noreply.github.com>
2023-09-09 01:42:20 +0300

    Test 1 and 2 done

--- tests/test_app1.py
+++ tests/test_app1.py
@@ -1,29 +1,20 @@
 from flask import Flask, request, jsonify
 from flask.testing import FlaskClient
-# from app import app
+from app import app
 import unittest
-
 
 class TestHelloWorld(unittest.TestCase):
     def setUp(self):
         # Create a test client
-        self.app = Flask(__name__)
-        self.client = self.app.test_client()
-
-        #Define the route '/' with a view function
-        @self.app.route('/')
-        def helloworld():
-            data = {"data": "Hello World"}
-            return jsonify(data)
+        self.client = app.test_client()
 
     def tearDown(self):
         pass  # Clean up resources if needed
 
     def test_helloworld(self):
-        with self.app.test_request_context('/'):
-            response = self.client.get('/')
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json, {"data": "Hello World"})
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"data": "Hello World"})
 
 
 if __name__ == "__main__":
--- tests/test_app2.py
+++ tests/test_app2.py
@@ -1,35 +1,49 @@
 from flask import Flask, request, jsonify
 from flask.testing import FlaskClient
-
-
-# Create a test client
-app = Flask(__name__)
-client = app.test_client()
-
+from app import app
+import unittest
 
 restaurants = [
     {
         "city": "Helsinki",
         "currency": "EUR",
         "delivery_price": 390,
-        "description": "Japanese ramen at its best",
+        "description": "Japanilaista ramenia parhaimmillaan",
         "image": "https://prod-wolt-venue-images-cdn.wolt.com/5d108aa82e757db3f4946ca9/d88ebd36611a5e56bfc6a60264fe3f81",
         "location": [24.941786527633663, 60.169934599421396],
         "name": "Momotoko Citycenter",
         "online": False,
         "tags": ["ramen", "risotto"],
         "blurhash": "j2DUFG8jbu8AXuLIT5Tt0B01R2;;"
+    },
+    {
+        "city": "Helsinki",
+        "currency": "EUR",
+        "delivery_price": 390,
+        "description": "Kiinalaista buffetia parhaimmillaan",
+        "image": "https://prod-wolt-venue-images-cdn.wolt.com/5d108aa82e757db3f4946ca9/d88ebd36611a5e56bfc6a60264fe3f81",
+        "location": [24.9677737, 60.2029952],
+        "name": "Kuwano",
+        "online": False,
+        "tags": ["chinese", "buffeti"],
+        "blurhash": "j2DUFG8jbu8AXuLIT5Tt0B01R2;;"
     }
 ]
 
-@app.route("/restaurants", methods=["GET"])
-def get_restaurant():
-    return jsonify(restaurants)
 
-def test_get_restaurant():
-    response = client.get('/restaurants')
-    assert response.status_code == 200
-    assert response.json == restaurants
+class TestRestaurants(unittest.TestCase):
+    def setUp(self):
+        # Create a test client
+        self.client = app.test_client()
+
+    def tearDown(self):
+        pass  # Clean up resources if needed
+
+    def test_get_restaurant(self):
+        response = self.client.get('/restaurants')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, restaurants)
+
 
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
